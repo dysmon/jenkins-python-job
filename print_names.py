cfd74fd5c58256fed2a92c8c9975641b642da33e
Dias <120464230+dysmon@users.noreply.github.com>
2025-02-21 19:51:37 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,1 +1,8 @@
-print("MODERN TECHNIQUES OF SOFTWARE DEVELOPMENT")
+import argparse
+
+parser = argparse.ArgumentParser(description="Print a custom message.")
+parser.add_argument("message", type=str, help="The message to print")
+
+args = parser.parse_args()
+
+print(args.message)
